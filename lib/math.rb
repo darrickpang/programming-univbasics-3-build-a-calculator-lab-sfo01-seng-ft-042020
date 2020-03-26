c31ebf9a299e82f871d569abdfa2723a1b358078
Darrick Pang <dkypang@yahoo.com>
2020-03-26 21:32:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  puts difference
 end
 
 def division(num1, num2)
-
+  divide = num1/num2
+  puts divide
 end
 
 def multiplication(num1, num2)
-
+  product = num1*num2
+  puts product
 end
 
 def modulo(num1, num2)
-
+  remain = num1 % num2
+  puts remain
 end
 
 def square_root(num)
-
+  root = Math.root(num)
+  puts root
 end
